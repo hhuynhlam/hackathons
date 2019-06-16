0a8e46f0ff4cd6e1d6ede54d05a984bf79707ae0
Hai Huynhlam <hhuynhlam@gmail.com>
2019-06-15 17:53:33 -0700

    chore: add eslint, react-native-elements, mock-api

---
+++ search_by_image/app/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: 'babel-eslint',
+
+  extends: [
+    'standard',
+    'standard-react',
+  ],
+
+  plugins: [
+    'jest',
+  ],
+
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+
+  "env": {
+    "jest/globals": true,
+  },
+
+  globals: {
+    __DEV__: true,
+  },
+
+  rules: {
+    'react/prop-types': ['off'],
+  },
+}
